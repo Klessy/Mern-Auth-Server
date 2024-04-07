5818f2b4e5294100e7ab6b4487aa3147508ef8aa
Precious Eruteya <eruteyaprecious@gmail.com>
2024-04-07 20:22:28 +0100

    Connected to MongoDb

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+
+
+
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDb");
+}).catch((err) => {
+    console.log(err);
+})
+
+
+
+
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log("Server started at 3000");
+})
